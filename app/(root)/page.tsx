2e264cbf5dcb938b9f922dca8e31fe200c536341
mtm-minaungpaing <minaungpaing@metateammyanmar.com>
2023-11-21 01:34:12 +0630

    add sample plugin

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-red-500 font-bold">Hello Notion Clone</h1>
+      <Button className="bg-emerald-500 text-white">Click Me!</Button>
+    </>
+  );
+}
