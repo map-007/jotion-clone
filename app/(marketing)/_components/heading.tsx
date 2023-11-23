943f5fc632cc45b563a5c0b5055b5f0fb8ad4125
map-007 <min.aungpaing007@gmail.com>
2023-11-24 01:09:07 +0630

    add theme and layout

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
 const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
@@ -10,6 +13,10 @@ const Heading = () => {
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Jotion is the connected workspace where <br/> better, faster work happens.
       </h3>
+      <Button>
+        Enter Jotion
+        <ArrowRight className="h-4 w-4 ml-2"/>
+      </Button>
     </div>
   );
 };
